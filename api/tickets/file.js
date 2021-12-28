89c5157e481b4bf464f955abbe48990442e7086c
CodyIsTheSenate <mathieuman78@gmail.com>
2021-12-28 12:24:12 +0100

    Add modification for files

--- api/tickets/file.js
+++ api/tickets/file.js
@@ -18,6 +18,100 @@ function makeid(length, filename) {
 
 /**
  * @swagger
+ * /ticket/{id}/file:
+ *   get:
+ *     summary: Get the list of files from a ticket 
+ *     tags: [Ticket]
+ *     parameters:
+ *     - name: dvflCookie
+ *       in: header
+ *       description: Cookie of the user making the request
+ *       required: true
+ *       type: string
+ *     - name: "id"
+ *       in: "path"
+ *       description: "Id of the ticket"
+ *       required: true
+ *       type: "integer"
+ *       format: "int64"
+ *     responses:
+ *       "200":
+ *         description: Get the list of files from a ticket 
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: "integer"
+ *                   format: "int64"
+ *                 filename:
+ *                   type: string
+ *                 comment:
+ *                   type: string
+ *                 isValid:
+ *                   type: boolean
+ *                 creationDate:
+ *                   type: "string"
+ *                   format: "date-time"
+ *                 modificationDate:
+ *                   type: "string"
+ *                   format: "date-time"
+ *       400:
+ *        description: "The body does not have all the necessary field"
+ *       401:
+ *        description: "The user is unauthenticated"
+ *       403:
+ *        description: "The user is not allowed"
+ *       500:
+ *        description: "Internal error with the request"
+ */
+
+module.exports.getListOfFile = async (app) => {
+    app.get('/api/ticket/:id/file', async (req, res) => {
+        try {
+            const dvflcookie = req.headers.dvflcookie;
+            const idTicket = req.params.id;
+            // unauthenticated user
+            if (!dvflcookie) {
+                res.sendStatus(401);
+                return;
+            }
+            const userIdAgent = app.cookiesList[req.headers.dvflcookie];
+
+            const resGetUserTicket = await app.executeQuery(app.db, "SELECT `i_idUser` AS 'id' FROM `printstickets` WHERE i_id = ?", [idTicket]);
+            if (resGetUserTicket[0] || resGetUserTicket[1].length !== 1) {
+                console.log(resGetUserTicket[0]);
+                res.sendStatus(500);
+                return;
+            }
+
+            const idTicketUser = resGetUserTicket[1][0].id;
+            if (idTicketUser != userIdAgent) {
+                const authViewResult = await require("../../functions/userAuthorization").validateUserAuth(app, userIdAgent, "myFabAgent");
+                if (!authViewResult) {
+                    res.sendStatus(403);
+                    return;
+                }
+            }
+
+            const dbRes = await app.executeQuery(app.db, "SELECT `i_id` AS 'id', `v_fileName` AS 'filename', `v_comment` AS 'comment', `b_valid` AS 'isValid', `dt_creationDate` AS 'creationDate', `dt_modificationDate` AS 'modificationDate' FROM `ticketfiles` WHERE `i_idTicket` = ?", [idTicket]);
+            if (dbRes[0]) {
+                console.log(dbRes[0]);
+                res.sendStatus(500);
+                return;
+            }
+            res.json(dbRes[1])
+        } catch (err) {
+            console.log("ERROR: GET /api/ticket/:id/file/");
+            console.log(err);
+            res.sendStatus(500);
+        }
+    });
+}
+
+/**
+ * @swagger
  * /file/{id}:
  *   get:
  *     summary: Download a file from a ticket
@@ -93,6 +187,7 @@ module.exports.getOneFile = async (app) => {
             if (fs.existsSync(__dirname + "/../../data/files/stl/" + resGetUserTicket[1][0].fileServerName)) res.download(__dirname + "/../../data/files/stl/" + resGetUserTicket[1][0].fileServerName, resGetUserTicket[1][0].fileName);
             else res.sendStatus(204);
         } catch (err) {
+            console.log("ERROR: GET /api/file/:id/");
             console.log(err);
             res.sendStatus(500);
         }
@@ -148,7 +243,7 @@ module.exports.getOneFile = async (app) => {
  */
 
 module.exports.post = async (app) => {
-    app.post('/api/ticket/:id/file', async (req, res) => {
+    app.post('/api/ticket/:id/file/', async (req, res) => {
         try {
             // The body does not have all the necessary field
             if (!req.files) {
@@ -231,6 +326,90 @@ module.exports.post = async (app) => {
             //return response
             res.sendStatus(200);
         } catch (err) {
+            console.log("ERROR: POST /api/ticket/:id/file/");
+            console.log(err);
+            res.sendStatus(500);
+        }
+    });
+}
+
+/**
+ * @swagger
+ * /file/{id}:
+ *   put:
+ *     summary: Change comment and isValid for a ticket
+ *     tags: [Ticket]
+ *     parameters:
+ *     - name: dvflCookie
+ *       in: header
+ *       description: Cookie of the user making the request
+ *       required: true
+ *       type: string
+ *     - name: "id"
+ *       in: "path"
+ *       description: "Id of the file"
+ *       required: true
+ *       type: "integer"
+ *       format: "int64"
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               comment:
+ *                 type: string
+ *               isValid:
+ *                 type: boolean
+ *     responses:
+ *       200:
+ *         description: "Modification saved successfully"
+ *       400:
+ *        description: "The body does not have all the necessary field"
+ *       401:
+ *        description: "The user is unauthenticated"
+ *       403:
+ *        description: "The user is not allowed"
+ *       500:
+ *        description: "Internal error with the request"
+ */
+
+module.exports.put = async (app) => {
+    app.put('/api/file/:id/', async (req, res) => {
+        try {
+            const dvflcookie = req.headers.dvflcookie;
+            // unauthenticated user
+            if (!dvflcookie) {
+                res.sendStatus(401);
+                return;
+            }
+            if (!req.params.id, !req.body.comment, !req.body.isValid) {
+                res.sendStatus(400);
+                return;
+            }
+            const idTicket = req.params.id;
+            const userIdAgent = app.cookiesList[req.headers.dvflcookie];
+
+            const authViewResult = await require("../../functions/userAuthorization").validateUserAuth(app, userIdAgent, "myFabAgent");
+            if (!authViewResult) {
+                res.sendStatus(403);
+                return;
+            }
+
+            const resUpdateFile = await app.executeQuery(app.db, "UPDATE `ticketfiles` SET `v_comment` = ?, `b_valid` = ? WHERE `i_id` = ?", [req.body.comment, req.body.isValid, idTicket]);
+            if (resUpdateFile[0]) {
+                console.log(resUpdateFile[0]);
+                res.sendStatus(500);
+                return;
+            } else if (resUpdateFile[0] || resUpdateFile[1].affectedRows !== 1) {
+                res.sendStatus(204);
+                return;
+            }
+
+            //return response
+            res.sendStatus(200);
+        } catch (err) {
+            console.log("ERROR: PUT /api/file/:id/");
             console.log(err);
             res.sendStatus(500);
         }
