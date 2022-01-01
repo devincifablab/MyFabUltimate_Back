00f6c87658d3173beebef6d45e4fd9d5fc5736d2
CodyIsTheSenate <mathieuman78@gmail.com>
2022-01-01 19:35:38 +0100

    Add mail function

--- functions/startApi.js
+++ functions/startApi.js
@@ -21,67 +21,6 @@ async function runFolder(path, app) {
                         console.log(error);
                     }
                 }
-
-                /*
-                if (route.getAll != null) {
-                    app.get(path + "/" + fileSplited[0], async function (req, res) {
-                        try {
-                            route.getAll(req, res, app);
-                        } catch (error) {
-                            console.log(error);
-                        }
-                    })
-                }
-                if (route.get != null) {
-                    const routeOptions = route.option ? route.option.get ? route.option.get : "" : "";
-                    app.get(path + "/" + fileSplited[0] + routeOptions, async function (req, res) {
-                        try {
-                            route.get(req, res, app);
-                        } catch (error) {
-                            console.log(error);
-                        }
-                    })
-                }
-                if (route.post != null) {
-                    const routeOptions = route.option ? route.option.post ? route.option.post : "" : "";
-                    app.post(path + "/" + fileSplited[0] + routeOptions, async function (req, res) {
-                        try {
-                            route.post(req, res, app);
-                        } catch (error) {
-                            console.log(error);
-                        }
-                    })
-                }
-                if (route.putMe != null) {
-                    app.put(path + "/" + fileSplited[0], async function (req, res) {
-                        try {
-                            route.putMe(req, res, app);
-                        } catch (error) {
-                            console.log(error);
-                        }
-                    })
-                }
-                if (route.put != null) {
-                    const routeOptions = route.option ? route.option.put ? route.option.put : "" : "";
-                    app.put(path + "/" + fileSplited[0] + routeOptions, async function (req, res) {
-                        try {
-                            route.put(req, res, app);
-                        } catch (error) {
-                            console.log(error);
-                        }
-                    })
-                }
-                if (route.delete != null) {
-                    const routeOptions = route.option ? route.option.delete ? route.option.delete : "" : "";
-                    app.delete(path + "/" + fileSplited[0] + routeOptions, async function (req, res) {
-                        try {
-                            route.delete(req, res, app);
-                        } catch (error) {
-                            console.log(error);
-                        }
-                    })
-                }
-                */
             }
         }
     });
