146d10ee20a70779125ad8007f04cadf88255f29
TOURNEUX Elias <55650958+eliasto@users.noreply.github.com>
2022-01-13 12:52:52 +0100

    You can now connect with your Microsoft account!
    Code was modified to add data from the Microsoft API

--- api/user.js
+++ api/user.js
@@ -176,7 +176,7 @@ module.exports.getMe = async (app) => {
                 res.sendStatus(401);
                 return;
             }
-            const dbRes = await app.executeQuery(app.db, 'SELECT `i_id` AS `id`, `v_firstName` AS "firstName", `v_lastName` AS "lastName", `v_email` AS "email", `dt_creationdate` AS "creationDate", `v_discordid` AS "discordid",`v_language` AS "language", (SELECT CASE WHEN dt_ruleSignature IS NULL THEN FALSE ELSE TRUE END FROM users WHERE `i_id` = ?) AS "acceptedRule", `b_mailValidated` AS "mailValidated" FROM `users` WHERE `i_id` = ? AND `b_deleted` = 0 AND `b_visible` = 1', [userId, userId]);
+            const dbRes = await app.executeQuery(app.db, 'SELECT `i_id` AS `id`, `v_firstName` AS "firstName", `v_lastName` AS "lastName", `v_email` AS "email", `dt_creationdate` AS "creationDate", `v_discordid` AS "discordid",`v_language` AS "language", `b_isMicrosoft` AS "isMicrosoft", `v_title` AS "title", (SELECT CASE WHEN dt_ruleSignature IS NULL THEN FALSE ELSE TRUE END FROM users WHERE `i_id` = ?) AS "acceptedRule", `b_mailValidated` AS "mailValidated" FROM `users` WHERE `i_id` = ? AND `b_deleted` = 0 AND `b_visible` = 1', [userId, userId]);
             // The sql request has an error
             if (dbRes[0]) {
                 console.log(dbRes[0]);
