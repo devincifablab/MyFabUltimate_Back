a657287686fbf7279c0df661605d3abc062f1f31
CodyIsTheSenate <mathieuman78@gmail.com>
2022-02-23 16:28:57 +0100

    Add refactoring for register

--- api/user/register.js
+++ api/user/register.js
@@ -52,70 +52,105 @@ function makeid(length) {
  *        description: "Internal error with the request"
  */
 
-module.exports.post = async (app) => {
+module.exports.postRegister = postRegister;
+async function postRegister(data) {
+    // The body does not have all the necessary field
+    if (!data.body.firstName || !data.body.lastName || !data.body.email || !data.body.password || validateEmail(data.body.email) === null) {
+        return {
+            type: "code",
+            code: 400
+        }
+    }
+    const queryCheckIfEmailExist = `SELECT 1 FROM users
+                                WHERE v_email = ?;`;
+    const resTestIfAccountExist = await data.app.executeQuery(data.app.db, queryCheckIfEmailExist, [data.body.email]);
+    // Error with the sql request
+    if (resTestIfAccountExist[0]) {
+        console.log(resTestIfAccountExist[0]);
+        return {
+            type: "code",
+            code: 500
+        }
+    }
+    if (resTestIfAccountExist[1].length !== 0) {
+        return {
+            type: "code",
+            code: 401
+        }
+    }
+
+    const language = data.body.language ? data.body.language : "fr";
+    const queryInsert = `INSERT INTO users (v_firstName, v_lastName, v_email, v_password, v_language)
+                        VALUES (?, ?, ?, ?, ?);`;
+    const resInsertNewAccount = await data.app.executeQuery(data.app.db, queryInsert, [data.body.firstName, data.body.lastName, data.body.email, sha256(data.body.password), language]);
+    // Error with the sql request
+    if (resInsertNewAccount[0] || resInsertNewAccount[1].affectedRows !== 1) {
+        console.log(resInsertNewAccount[0]);
+        return {
+            type: "code",
+            code: 500
+        }
+    }
+    const queryLastInsert = `SELECT LAST_INSERT_ID() AS 'id';`;
+    const resGetIdUserInserted = await data.app.executeQuery(data.app.db, queryLastInsert, []);
+    // Error with the sql request
+    if (resGetIdUserInserted[0] || resGetIdUserInserted[1].length !== 1 || resGetIdUserInserted[1][0].id === 0) {
+        console.log(resGetIdUserInserted[0]);
+        return {
+            type: "code",
+            code: 500
+        }
+    }
+    const idNewUser = resGetIdUserInserted[1][0].id;
+
+    let tocken = null;
+    while (tocken == null) {
+        const testTocken = makeid(10);
+        const querySelectTocken = `SELECT 1 FROM mailtocken
+                                WHERE v_value = ?`;
+        const resTestTocken = await data.app.executeQuery(data.app.db, querySelectTocken, [testTocken]);
+        if (resTestTocken[0]) {
+            console.log(resTestTocken[0]);
+            return {
+                type: "code",
+                code: 500
+            }
+        }
+        if (resTestTocken[1]) tocken = testTocken;
+    }
+
+    const sendMail = data.body.sendMail == null ? true : data.body.sendMail;
+
+    const queryInsertTocken = `INSERT INTO mailtocken (i_idUser, v_value, b_mailSend)
+                            VALUES (?, ?, ?);`;
+    const resInsertTocken = await data.app.executeQuery(data.app.db, queryInsertTocken, [idNewUser, tocken, sendMail ? "1" : "0"]);
+    if (resInsertTocken[0]) {
+        console.log(resInsertTocken[0]);
+        return {
+            type: "code",
+            code: 500
+        }
+    }
+
+    //Send validation email to the user
+    if (sendMail) {
+        data.sendMailFunction.sendMail(data.body.email, "[MyFab] Validation de mail", "Bonjour,\nPour valider votre mail merci de cliquer sur ce lien\n" + tocken);
+    }
+
+    return {
+        type: "code",
+        code: 200
+    }
+}
+
+module.exports.startApi = startApi;
+async function startApi(app) {
     app.post("/api/user/register/", async function (req, res) {
         try {
-            // The body does not have all the necessary field
-            if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.password || validateEmail(req.body.email) === null) {
-                res.sendStatus(400);
-                return;
-            }
-            const resTestIfAccountExist = await app.executeQuery(app.db, "SELECT 1 FROM `users` WHERE v_email = ?;", [req.body.email]);
-            // Error with the sql request
-            if (resTestIfAccountExist[0]) {
-                console.log(resTestIfAccountExist[0]);
-                res.sendStatus(500);
-                return;
-            }
-            if (resTestIfAccountExist[1].length !== 0) {
-                res.sendStatus(401);
-                return;
-            }
-
-            const language = req.body.language ? req.body.language : "fr";
-            const resInsertNewAccount = await app.executeQuery(app.db, "INSERT INTO `users` (`v_firstName`, `v_lastName`, `v_email`, `v_password`, `v_language`) VALUES (?, ?, ?, ?, ?);", [req.body.firstName, req.body.lastName, req.body.email, sha256(req.body.password), language]);
-            // Error with the sql request
-            if (resInsertNewAccount[0] || resInsertNewAccount[1].affectedRows !== 1) {
-                console.log(resInsertNewAccount[0]);
-                res.sendStatus(500);
-                return;
-            }
-            const resGetIdUserInserted = await app.executeQuery(app.db, "SELECT LAST_INSERT_ID() AS 'id';", []);
-            // Error with the sql request
-            if (resGetIdUserInserted[0] || resGetIdUserInserted[1].length !== 1 || resGetIdUserInserted[1][0].id === 0) {
-                console.log(resGetIdUserInserted[0]);
-                res.sendStatus(500);
-                return;
-            }
-            const idNewUser = resGetIdUserInserted[1][0].id;
-
-            let tocken = null;
-            while (tocken == null) {
-                const testTocken = makeid(10);
-                const resTestTocken = await app.executeQuery(app.db, "SELECT 1 FROM `mailtocken` WHERE v_value = ?", [testTocken]);
-                if (resTestTocken[0]) {
-                    console.log(resTestTocken[0]);
-                    res.sendStatus(500);
-                    return;
-                }
-                if (resTestTocken[1]) tocken = testTocken;
-            }
-
-            const sendMail = req.body.sendMail == null ? true : req.body.sendMail;
-
-            const resInsertTocken = await app.executeQuery(app.db, "INSERT INTO `mailtocken` (`i_idUser`, `v_value`, `b_mailSend`) VALUES (?, ?, ?);", [idNewUser, tocken, sendMail ? "1" : "0"]);
-            if (resInsertTocken[0]) {
-                console.log(resInsertTocken[0]);
-                res.sendStatus(500);
-                return;
-            }
-
-            //Send validation email to the user
-            if (sendMail) {
-                require('../../functions/sendMail').sendMail(req.body.email, "[MyFab] Validation de mail", "Bonjour,\nPour valider votre mail merci de cliquer sur ce lien\n" + tocken);
-            }
-
-            res.sendStatus(200);
+            const data = await require("../../functions/apiActions").prepareData(app, req, res);
+            data.sendMailFunction = require('../../functions/sendMail');
+            const result = await postRegister(data);
+            await require("../../functions/apiActions").sendResponse(req, res, result);
         } catch (error) {
             console.log("ERROR: POST /user/register/");
             console.log(error);
