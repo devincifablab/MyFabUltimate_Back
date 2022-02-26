52cd0e504022faecc244b8abcc6862dd682e6290
CodyIsTheSenate <mathieuman78@gmail.com>
2022-02-27 00:00:53 +0100

    Fix and create root user for prepareDb

--- importSqlTables.js
+++ importSqlTables.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
+const sha256 = require("sha256");
 
 
 async function importSqlTables(file) {
     const db = await require("./functions/dataBase/createConnection").open();
     const executeQuery = require("./functions/dataBase/executeQuery").run;
-    await new Promise((resolve, reject) => {
+    const dbName = require("./config.json").db.database;
+    await executeQuery(db, "CREATE DATABASE IF NOT EXISTS ??", [dbName]);
+    await executeQuery(db, "USE ??", [dbName]);
+    await new Promise((resolve) => {
         fs.readFile(file, 'utf8', async (err, data) => {
             if (err) {
                 console.error(err)
@@ -17,16 +21,41 @@ async function importSqlTables(file) {
                     if (res[0] && res[0].sql.length > 5) console.log(res[0]);
                 }
             }
-    
+
             await require("./functions/dataBase/createConnection").close(db);
             resolve();
         })
     })
 }
 
+async function addRootUser() {
+    const db = await require("./functions/dataBase/createConnection").open();
+    const executeQuery = require("./functions/dataBase/executeQuery").run;
+    const dbName = require("./config.json").db.database;
+    await executeQuery(db, "CREATE DATABASE IF NOT EXISTS ??", [dbName]);
+    await executeQuery(db, "USE ??", [dbName]);
+
+    const rootPassWord = require("./functions/makeid").makeid(30);
+    const queryInsertRootUser = `INSERT INTO users
+                            (v_firstName, v_lastName, v_email, v_password, dt_creationdate, v_language, dt_ruleSignature, b_deleted, b_visible, b_mailValidated, b_isMicrosoft, v_title)
+                            VALUES
+                            ('root', 'root', 'root@root.com', ?, current_timestamp(), 'fr', current_timestamp(), '0', '0', '1', '0', 'Root');`
+    await executeQuery(db, queryInsertRootUser, [sha256(rootPassWord)]);
+    const queryInsertRoolRole = `INSERT INTO rolescorrelation (i_idUser, i_idRole)
+                                VALUES ((SELECT i_id FROM users WHERE v_email = 'root@root.com'),
+                                (SELECT i_id FROM gd_roles WHERE v_name = 'Administrateur'))`;
+    await executeQuery(db, queryInsertRoolRole, []);
+    await fs.writeFileSync("./data/defaultRootPassword", rootPassWord);
+    await require("./functions/dataBase/createConnection").close(db);
+    return
+}
+
+
+
 async function start() {
     await importSqlTables('./myFabUltimateDb.sql');
     await importSqlTables('./globalData.sql');
+    await addRootUser();
     console.log("The database is ready");
 }
 
