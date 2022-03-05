c8a31cebfef236fd2a56e74e50b64c2b257959cc
CodyIsTheSenate <mathieuman78@gmail.com>
2022-03-05 17:03:52 +0100

    Cant insert duplicate elements for prepareDb

--- importSqlTables.js
+++ importSqlTables.js
@@ -40,14 +40,18 @@ async function addRootUser() {
                             (v_firstName, v_lastName, v_email, v_password, dt_creationdate, v_language, dt_ruleSignature, b_deleted, b_visible, b_mailValidated, b_isMicrosoft, v_title)
                             VALUES
                             ('root', 'root', 'root@root.com', ?, current_timestamp(), 'fr', current_timestamp(), '0', '0', '1', '0', 'Root');`
-    await executeQuery(db, queryInsertRootUser, [sha256(rootPassWord)]);
+    const res = await executeQuery(db, queryInsertRootUser, [sha256(rootPassWord)]);
+    if (res[0]) {
+        await require("./functions/dataBase/createConnection").close(db);
+        return;
+    }
     const queryInsertRoolRole = `INSERT INTO rolescorrelation (i_idUser, i_idRole)
                                 VALUES ((SELECT i_id FROM users WHERE v_email = 'root@root.com'),
                                 (SELECT i_id FROM gd_roles WHERE v_name = 'Administrateur'))`;
     await executeQuery(db, queryInsertRoolRole, []);
     await fs.writeFileSync("./data/defaultRootPassword", rootPassWord);
     await require("./functions/dataBase/createConnection").close(db);
-    return
+    return;
 }
 
 
