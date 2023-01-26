2748a1144f565a9a614f5c71ebee13c5d9ad60f0
CodyIsTheSenate <mathieuman78@gmail.com>
2023-01-26 17:27:09 +0100

    Fix error with sql import

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,7 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
@@ -193,5 +193,5 @@ module.exports = {
   // watchman: true,
   globalSetup: "./tests/jestGlobalSetup.js",
   globalTeardown: "./tests/jestGlobalTeardown.js",
-  setupFilesAfterEnv: ['@alex_neo/jest-expect-message'],
+  setupFilesAfterEnv: ["@alex_neo/jest-expect-message"],
 };
