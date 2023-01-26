fb293648ce0485ca5d440bc03a9ac38b3bf229da
CodyIsTheSenate <mathieuman78@gmail.com>
2023-01-26 18:00:51 +0100

    Fix errors tests/api/cookie.test.js

--- tests/api/cookie.test.js
+++ tests/api/cookie.test.js
@@ -1,148 +1,77 @@
-describe('GET /api/cookie/', () => {
-    test('200', async () => {
-        const data = {
-            specialcode: "testCode",
-            userAuthorization: {
-                getSpecialCode: async () => {
-                    return "testCode"
-                }
-            },
-            app: {}
-        };
-        const response = await require("../../api/cookie.js").cookieTestSpecialCode(data);
+describe("GET /api/cookie/", () => {
+  test("200", async () => {
+    const data = {
+      specialcode: "testCode",
+      userAuthorization: {
+        checkSpecialCode: async () => {
+          return true;
+        },
+      },
+      app: {},
+    };
+    const response = await require("../../api/cookie.js").cookieTestSpecialCode(data);
 
-        expect(response.code).toBe(200);
-        expect(response.type).toBe("code");
-    })
+    expect(response.code).toBe(200);
+    expect(response.type).toBe("code");
+  });
 
-    test('404-noSpecialCode', async () => {
-        const data = {
-            specialcode: "testCode",
-            userAuthorization: {
-                getSpecialCode: async () => {
-                    return null
-                }
-            },
-            app: {}
-        };
-        const response = await require("../../api/cookie.js").cookieTestSpecialCode(data);
+  test("404-invalidCode", async () => {
+    const data = {
+      specialcode: "testCode",
+      userAuthorization: {
+        checkSpecialCode: async () => {
+          return false;
+        },
+      },
+      app: {},
+    };
+    const response = await require("../../api/cookie.js").cookieTestSpecialCode(data);
 
-        expect(response.code).toBe(404);
-        expect(response.type).toBe("code");
-    })
+    expect(response.code).toBe(404);
+    expect(response.type).toBe("code");
+  });
+});
 
-    test('404-noSpecialCodeUser', async () => {
-        const data = {
-            specialcode: null,
-            userAuthorization: {
-                getSpecialCode: async () => {
-                    return "testCode"
-                }
-            },
-            app: {}
-        };
-        const response = await require("../../api/cookie.js").cookieTestSpecialCode(data);
+describe("DELETE /api/cookie/", () => {
+  test("200", async () => {
+    const data = {
+      specialcode: "testCode",
+      userAuthorization: {
+        checkSpecialCode: async () => {
+          return true;
+        },
+      },
+      app: {
+        cookiesList: {
+          value: "test",
+        },
+      },
+    };
+    const response = await require("../../api/cookie.js").cookieDeleteAll(data);
 
-        expect(response.code).toBe(404);
-        expect(response.type).toBe("code");
-    })
+    expect(response.code).toBe(200);
+    expect(response.type).toBe("code");
+    expect(Object.keys(data.app.cookiesList).length).toBe(0);
+  });
 
-    test('404-specialCodesAreDifferents', async () => {
-        const data = {
-            specialcode: "testCode",
-            userAuthorization: {
-                getSpecialCode: async () => {
-                    return "otherTestCode"
-                }
-            },
-            app: {}
-        };
-        const response = await require("../../api/cookie.js").cookieTestSpecialCode(data);
+  test("404-invalidCode", async () => {
+    const data = {
+      specialcode: "testCode",
+      userAuthorization: {
+        checkSpecialCode: async () => {
+          return false;
+        },
+      },
+      app: {
+        cookiesList: {
+          value: "test",
+        },
+      },
+    };
+    const response = await require("../../api/cookie.js").cookieDeleteAll(data);
 
-        expect(response.code).toBe(404);
-        expect(response.type).toBe("code");
-    })
-})
-
-describe('DELETE /api/cookie/', () => {
-    test('200', async () => {
-        const data = {
-            specialcode: "testCode",
-            userAuthorization: {
-                getSpecialCode: async () => {
-                    return "testCode"
-                }
-            },
-            app: {
-                cookiesList: {
-                    value: "test"
-                }
-            }
-        };
-        const response = await require("../../api/cookie.js").cookieDeleteAll(data);
-
-        expect(response.code).toBe(200);
-        expect(response.type).toBe("code");
-        expect(Array.from(data.app.cookiesList).length).toBe(0);
-    })
-
-    test('404-noSpecialCode', async () => {
-        const data = {
-            specialcode: "testCode",
-            userAuthorization: {
-                getSpecialCode: async () => {
-                    return null
-                }
-            },
-            app: {
-                cookiesList: {
-                    value: "test"
-                }
-            }
-        };
-        const response = await require("../../api/cookie.js").cookieDeleteAll(data);
-
-        expect(response.code).toBe(404);
-        expect(response.type).toBe("code");
-    })
-
-    test('404-noSpecialCodeUser', async () => {
-        const data = {
-            specialcode: null,
-            userAuthorization: {
-                getSpecialCode: async () => {
-                    return "testCode"
-                }
-            },
-            app: {
-                cookiesList: {
-                    value: "test"
-                }
-            }
-        };
-        const response = await require("../../api/cookie.js").cookieDeleteAll(data);
-
-        expect(response.code).toBe(404);
-        expect(response.type).toBe("code");
-    })
-
-    test('404-specialCodesAreDifferents', async () => {
-        const data = {
-            specialcode: "testCode",
-            userAuthorization: {
-                getSpecialCode: async () => {
-                    return "otherTestCode"
-                }
-            },
-            app: {
-                cookiesList: {
-                    value: "test"
-                }
-            }
-        };
-        const response = await require("../../api/cookie.js").cookieDeleteAll(data);
-
-        expect(response.code).toBe(404);
-        expect(response.type).toBe("code");
-    })
-})
+    expect(response.code).toBe(404);
+    expect(response.type).toBe("code");
+    expect(Object.keys(data.app.cookiesList).length).not.toBe(0);
+  });
+});
